c5466b0e00de2c3ce41f64fa4c74de6db5a84c6c
Camille <camillegomes74@gmail.com>
2025-03-02 18:02:38 -0300

    Feat: create collections Page

--- src/app/collections/page.tsx
+++ src/app/collections/page.tsx
@@ -1,8 +1,34 @@
+'use client';
 import React from "react";
+import NavBar from "@/app/components/NavBar";
+import { collectionsData } from "../data/collectionsData";
 
 export default function CollectionsPage() {
     return (
-       <>
-       </>
-    );
+        <>
+        <NavBar/>
+        <div className="mx-[90px] my-20">
+            <div>
+                <h1 className="font-abril text-white text-3xl sm:text-4xl lg:text-5xl 2xl:text-7xl">Discover more
+                    about our exhibition</h1>
+                <div className="w-full h-1 bg-[#F5F5F5B2] mt-2"></div>
+                <p className="text-white mt-5">
+                    Find out more about our collection and the artists behind the works. Our art exhibition is open
+                    to the public and we welcome you to visit our gallery.
+                </p>
+            </div>
+            <div className="grid grid-cols-3 gap-5 mt-10 collection-grid">
+                {collectionsData.map((collection, index) => (
+                    <div className="tile relative" key={index}>
+                         <img src={collection.src} alt={collection.alt} className="w-full h-full object-cover"/>
+                        <div className="absolute inset-x-0 bg-secondary opacity-0 hover:opacity-100 transition-opacity duration-300 flex flex-col justify-center items-start p-4 lg:h-1/2 2xl:h-1/4 bottom-0">
+                            <h2 className="text-black font-abril text-2xl">{collection.title}</h2>
+                            <p className="text-black mt-2">{collection.artist}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+</>
+);
 }
---
+++ src/app/data/collectionsData.ts
@@ -0,0 +1,80 @@
+export interface collectionData {
+    src: string;
+    alt: string;
+    title?: string;
+    artist?: string;
+}
+
+export const collectionsData: collectionData[] = [
+    {
+        src: "/collection1.png",
+        alt: "A close-up oil painting of a young woman with fair skin, wearing a blue and yellow turban and a large pearl earring. She gazes over her shoulder toward the viewer with wide eyes and slightly parted lips. The dark background contrasts with the soft light illuminating her face, enhancing her delicate features",
+        title: "The Conversion of St. Paul",
+        artist: "Caravaggio, 1601",
+    },
+    {
+        src: "/collection2.png",
+        alt: "A large-scale oil painting depicting a group of armed militiamen, led by Captain Frans Banning Cocq dressed in black with a red sash. The soldiers are captured in motion, some holding weapons while others engage in conversation.",
+        title: "The Milkmaid",
+        artist: "Johannes Vermeer, 1660",
+    },
+    {
+        src: "/slide1.png",
+        alt: "Girl with a Pearl Earring",
+        title: "Girl with a Pearl Earring",
+        artist: "Johannes Vermeer, 1665",
+    },
+    {
+        src: "/collection7.png",
+        alt: "",
+        title: "The Descent from the Cross",
+        artist: "Peter Paul Rubens, 1612",
+    },
+    {
+        src: "/collection4.png",
+        alt: "",
+        title: "Ecsasty of St. Teresa",
+        artist: "Gian Lorenzo Bernini, 1652",
+    },
+    {
+        src: "/collection5.png",
+        alt: "",
+        title: "The Garden of Eden with the Fall of Man",
+        artist: "Peter Paul Rubens, 1615",
+    },
+    {
+        src: "/collection6.png",
+        alt: "",
+        title: "The Death of Sardanapalus",
+        artist: "Eugène Delacroix, 1827",
+    },
+    {
+        src: "/slide2.png",
+        alt: "The Night Watch",
+        title: "The Night Watch",
+        artist: "Rembrandt van Rijn, 1642",
+    },
+    {
+        src: "/collection8.png",
+        alt: "",
+        title: "The Bull",
+        artist: "Paulus Potter, 1647",
+    },
+    {
+        src: "/collection9.png",
+        alt: "",
+        title: "Nymphs Filling the Cornucopia",
+        artist: "Jan Brueghel the Elder & Peter Paul Rubens, 1615",
+    },
+    {
+        src: "/slide3.png",
+        alt: "",
+        title: "The Anatomy Lesson of Dr. Nicolaes Tulp",
+        artist: "Rembrandt van Rijn, 1632",
+    },
+    {
+        src: "/slide4.png",
+        alt: "",
+        title: "As the Old Sing, So Pipe the Young",
+    },
+];
