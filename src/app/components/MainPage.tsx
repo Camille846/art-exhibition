a00bf1d55b952b9fec70d5e2dee0dd1774f04c2d
Camille <camillegomes74@gmail.com>
2025-03-03 10:47:12 -0300

    Feat: add Scroll Event Listener for Image Slider

--- src/app/components/MainPage.tsx
+++ src/app/components/MainPage.tsx
@@ -1,11 +1,42 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Image from 'next/image';
 import { FaArrowRightLong } from 'react-icons/fa6';
 import { mainPageData } from "../data/mainPageData";
 
 function MainPage() {
-    const [selectedImage, setSelectedImage] = useState(mainPageData[0]);
+    const [selectedImageIndex, setSelectedImageIndex] = useState(0);
+    const imageContainerRef = useRef<HTMLDivElement>(null);
+
+    const handleWheel = (event: WheelEvent) => {
+        event.preventDefault(); // Prevent default scroll behavior
+
+        const delta = Math.sign(event.deltaY);
+        let newIndex = selectedImageIndex + delta;
+
+        if (newIndex < 0) {
+            newIndex = mainPageData.length - 1;
+        } else if (newIndex >= mainPageData.length) {
+            newIndex = 0;
+        }
+
+        setSelectedImageIndex(newIndex);
+    };
+
+
+    useEffect(() => {
+        const container = imageContainerRef.current;
+        if (container) {
+            container.addEventListener('wheel', handleWheel);
+            return () => {
+                if (container) {
+                    container.removeEventListener('wheel', handleWheel);
+                }
+            };
+        }
+    }, [selectedImageIndex]);
+
+    const selectedImage = mainPageData[selectedImageIndex];
 
     return (
         <div
@@ -29,7 +60,8 @@ function MainPage() {
                     <div className="lg:h-[50vh] w-full h-auto mb-9">
                         {selectedImage && (
                             <div className="flex flex-col gap-2">
-                                <span className="text-secondary italic font-medium">On view in {selectedImage.room} until March 31, 2025</span>
+                                <span
+                                    className="text-secondary italic font-medium">On view in {selectedImage.room} until March 31, 2025</span>
                                 <h1 className="font-abril text-white text-3xl sm:text-4xl lg:text-5xl 2xl:text-7xl">{selectedImage.title}</h1>
                                 <p className="text-secondary">{selectedImage.artist}</p>
                                 <p className="text-white mt-2 sm:mt-4 text-xs md:text-sm font-semibold">
@@ -39,17 +71,24 @@ function MainPage() {
                         )}
                     </div>
                     <div className="flex gap-5 2xl:mt-10">
-                        <button className="bg-secondary text-black font-semibold px-4 rounded-md hover:opacity-80 cursor-pointer">visit now</button>
-                        <button className="text-white font-semibold px-4 py-2 rounded-md border-2 border-white flex gap-2 items-center hover:opacity-80 cursor-pointer">
+                        <button
+                            className="bg-secondary text-black font-semibold px-4 rounded-md hover:opacity-80 cursor-pointer">visit
+                            now
+                        </button>
+                        <button
+                            className="text-white font-semibold px-4 py-2 rounded-md border-2 border-white flex gap-2 items-center hover:opacity-80 cursor-pointer">
                             discover more
-                            <FaArrowRightLong />
+                            <FaArrowRightLong/>
                         </button>
                     </div>
-                    <div className="mt-5 2xl:mt-10 flex gap-2 lg:flex-row max-md:grid max-md:grid-cols-3 max-md:mb-20">
+                    <div
+                        className="mt-5 2xl:mt-10 flex gap-2 lg:flex-row max-md:grid max-md:grid-cols-3 max-md:mb-20"
+                        ref={imageContainerRef}>
                         {mainPageData.map((image, index) => (
                             <div key={index}
-                                 className="w-[100px] sm:w-[150px] h-[75px] sm:h-[100px] shadow-2xl cursor-pointer border-2 border-white"
-                                 onClick={() => setSelectedImage(image)}>
+                                 className={`w-[100px] sm:w-[150px] h-[75px] sm:h-[100px] shadow-2xl cursor-pointer border-2 ${selectedImageIndex === index ? 'border-secondary' : 'border-white'}`}
+                                 onClick={() => setSelectedImageIndex(index)}
+                            >
                                 <Image
                                     src={image.src}
                                     className="w-full h-full object-cover"
