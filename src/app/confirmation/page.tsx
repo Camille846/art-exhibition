4ff9cfaf6b6187ad1b2dd725d6a37b972d98fea6
Camille <camillegomes74@gmail.com>
2025-03-05 10:13:05 -0300

    Feat: responsive confirmation page

--- src/app/confirmation/page.tsx
+++ src/app/confirmation/page.tsx
@@ -6,27 +6,29 @@ import { useBooking } from "@/context/BookingContext";
 
 export default function ConfirmationPage() {
     const { Canvas } = useQRCode();
-    const { tickets, selectedDate, formData } = useBooking(); // Acesse os dados do contexto
+    const { tickets, selectedDate, personalInfo } = useBooking();
     const ticketTypes = tickets.filter(ticket => ticket.quantity > 0);
+
+    const fullName = `${personalInfo.firstName} ${personalInfo.lastName}`.trim()
 
     return (
         <>
             <NavBar />
             <div className="min-h-screen bg-black text-white">
-                <div className="mx-32 p-4 md:p-8">
-                    <h1 className="text-5xl mb-2 font-abril">Your ticket</h1>
+                <div className="mx-4 my-4 lg:my-0 lg:mx-32 p-4 md:p-8">
+                    <h1 className="text-xl lg:text-5xl my-2 font-abril w-full">Your ticket</h1>
                     <div className="w-full h-[1px] bg-[#F5F5F5B2]"></div>
 
                     <div className="relative z-10 bg-[url('/confirmed.png')] bg-cover bg-center bg-no-repeat w-full h-screen mt-10 flex justify-center items-center">
                         <div className="absolute inset-0 bg-black/50"></div>
                         <div className="w-full h-full shadow-2xl shadow-black flex items-center justify-center">
                             <div className="absolute z-20 flex flex-col bg-navBg p-8 rounded-xl max-w-[500px] gap-2 mt-24 mb-20">
-                                <img src={'/confirmed.png'} alt="confirmed" className="w-full h-[300px] rounded-lg mb-4" />
+                                <img src={'/confirmed.png'} alt="confirmed" className="w-full 2xl:h-[300px] h-[150px] rounded-lg mb-4  object-cover object-center" />
 
                                 {/* Dados do Ingresso */}
                                 <div className="flex justify-between">
                                     <div className="flex items-center gap-2">
-                                        <span className="text-secondary font-abril text-4xl">{selectedDate.toLocaleDateString("en-US", { day: "2-digit" })}</span>
+                                        <span className="text-secondary font-abril text-xl 2xl:text-4xl">{selectedDate.toLocaleDateString("en-US", { day: "2-digit" })}</span>
                                         <div className="flex flex-col border-l border-white pl-2">
                                             <span className="text-white font-abril text-md">{selectedDate.toLocaleDateString("en-US", { month: "long" })}</span>
                                             <span className="text-white font-abril text-md">{selectedDate.toLocaleDateString("en-US", { year: "numeric" })}</span>
@@ -35,7 +37,7 @@ export default function ConfirmationPage() {
                                     <div>
                                         {ticketTypes.map((ticket) => (
                                             <div key={ticket.name} className="flex justify-end">
-                                                <div className="text-right text-grey font-medium text-lg">
+                                                <div className="text-right text-grey font-medium 2xl:text-lg text-base">
                                                     <span className="block">{ticket.name} x {ticket.quantity}</span>
                                                 </div>
                                             </div>
@@ -44,8 +46,8 @@ export default function ConfirmationPage() {
                                 </div>
 
 
-                                <h1 className="text-5xl text-secondary font-abril my-3">Art Exhibition</h1>
-                                <p className="font-bold text-lg">{formData.firstName} {formData.lastName}</p>
+                                <h1 className="text-2xl 2xl:text-5xl text-secondary font-abril">Art Exhibition</h1>
+                                <p className="font-bold text-lg text-white">{fullName}</p>
                                 <p className="text-gray-400 text-xs">Tickets include admission to all galleries and special exhibitions.</p>
 
                                 <div className="w-full h-[1px] border-2 border-dashed border-grey my-2"></div>
